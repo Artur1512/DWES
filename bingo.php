328d1de26fa70e6e30907290755ef635d2ed7317
Artur1512 <63103545+Artur1512@users.noreply.github.com>
2021-10-07 20:07:47 +0200

    Update bingo.php

--- bingo.php
+++ bingo.php
@@ -2,13 +2,6 @@
     <HEAD>
         <TITLE> bingo </TITLE>
         <STYLE>
-            table, th, td{
-                background-color: #c6d6ff;
-                border:2px solid black;
-                border-collapse: collapse;
-                padding-left: 10px;
-                padding-right: 10px;
-            }
         </STYLE>
     </HEAD>
     <BODY>
